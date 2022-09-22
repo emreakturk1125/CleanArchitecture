02c396eb565b2d6b8d5595b1ec87a07377e75446
emreakturk1125 <akturk_emre1125@hotmail.com>
2022-09-22 21:36:19 +0300

    Development

---
+++ nArchitecture/src/demoProjects/rentACarProject/Application/Features/Models/Profiles/MappingProfiles.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.Models.Dtos;
+using AutoMapper;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Models.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Model,ModelListDto>().ReverseMap();   
+            CreateMap<IPaginate<Model>,ModelListDto>().ReverseMap();
+        }
+    }
+}
